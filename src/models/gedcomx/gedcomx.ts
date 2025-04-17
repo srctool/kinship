8544d4120713a28c1ba538af041a126d170dad03
Ibnul Mutaki <36250619+cacing69@users.noreply.github.com>
2025-04-17 16:42:46 +0700

    fix: remove unused model

--- src/models/gedcomx/gedcomx.ts
+++
@@ -1,9 +0,0 @@
-import { Person } from './person';
-import { Relationship } from './relationship';
-import { SourceDescription } from './source-description';
-
-export interface GedcomX {
-    persons?: Person[];
-    relationships?: Relationship[];
-    sourceDescriptions?: SourceDescription[];
-}
